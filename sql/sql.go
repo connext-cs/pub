31bb20b0835d1d7cadb3ff8a6d896bb9ddd9e151
trump.liu <kakaliush@push-tech.com>
2019-08-29 16:16:09 +0800

    bulk insert sql

---
+++ sql/sql.go
@@ -0,0 +1,125 @@
+package orm
+
+import "fmt"
+import "strings"
+
+
+func GetBulkInsertSqlStr(table string, columns []string, values ...[]interface{}) string {
+	columnStr := strings.Join(columns, ",")
+	sqlStr := fmt.Sprintf("INSERT INTO `%s` (%s) values ", table, columnStr)
+
+	columnCnt := len(values) - 1
+	for i := 0; i <= columnCnt; i++ {
+		oneColumn := ""
+		oneColumn += "("
+		cnt := len(values[i]) - 1
+		for j := 0; j <= cnt; j++ {
+			val := values[i][j]
+			switch val.(type) {
+			case string:
+				if j < cnt {
+					oneColumn += fmt.Sprintf("'%s',", val.(string))
+				} else {
+					if i < columnCnt {
+						oneColumn += fmt.Sprintf("'%s'),", val.(string))
+					} else {
+						oneColumn += fmt.Sprintf("'%s');", val.(string))
+					}
+				}
+				break
+			case int:
+				if j < cnt {
+					oneColumn += fmt.Sprintf("'%d',", val.(int))
+				} else {
+					if i < columnCnt {
+						oneColumn += fmt.Sprintf("'%d'),", val.(int))
+					} else {
+						oneColumn += fmt.Sprintf("'%d');", val.(int))
+					}
+				}
+				break
+			case int32:
+				if j < cnt {
+					oneColumn += fmt.Sprintf("'%d',", val.(int32))
+				} else {
+					if i < columnCnt {
+						oneColumn += fmt.Sprintf("'%d'),", val.(int32))
+					} else {
+						oneColumn += fmt.Sprintf("'%d');", val.(int32))
+					}
+				}
+				break
+			case int16:
+				if j < cnt {
+					oneColumn += fmt.Sprintf("'%d',", val.(int16))
+				} else {
+					if i < columnCnt {
+						oneColumn += fmt.Sprintf("'%d'),", val.(int16))
+					} else {
+						oneColumn += fmt.Sprintf("'%d');", val.(int16))
+					}
+				}
+				break
+			case int8:
+				if j < cnt {
+					oneColumn += fmt.Sprintf("'%d',", val.(int8))
+				} else {
+					if i < columnCnt {
+						oneColumn += fmt.Sprintf("'%d'),", val.(int8))
+					} else {
+						oneColumn += fmt.Sprintf("'%d');", val.(int8))
+					}
+				}
+				break
+			case int64:
+				if j < cnt {
+					oneColumn += fmt.Sprintf("'%d',", val.(int64))
+				} else {
+					if i < columnCnt {
+						oneColumn += fmt.Sprintf("'%d'),", val.(int64))
+					} else {
+						oneColumn += fmt.Sprintf("'%d');", val.(int64))
+					}
+				}
+				break
+			case float32:
+				if j < cnt {
+					oneColumn += fmt.Sprintf("'%f',", val.(float32))
+				} else {
+					if i < columnCnt {
+						oneColumn += fmt.Sprintf("'%f'),", val.(float32))
+					} else {
+						oneColumn += fmt.Sprintf("'%f');", val.(float32))
+					}
+				}
+				break
+			case float64:
+				if j < cnt {
+					oneColumn += fmt.Sprintf("'%f',", val.(float64))
+				} else {
+					if i < columnCnt {
+						oneColumn += fmt.Sprintf("'%f'),", val.(float64))
+					} else {
+						oneColumn += fmt.Sprintf("'%f');", val.(float64))
+					}
+				}
+				break
+			case uint, uint16, uint32, uint64:
+				if j < cnt {
+					oneColumn += fmt.Sprintf("'%d',", val.(uint))
+				} else {
+					if i < columnCnt {
+						oneColumn += fmt.Sprintf("'%d'),", val.(uint))
+					} else {
+						oneColumn += fmt.Sprintf("'%d');", val.(uint))
+					}
+				}
+				break
+			}
+
+		}
+		sqlStr += oneColumn
+	}
+	fmt.Print("insert sql is :\n", sqlStr)
+	return sqlStr
+}
---
+++ sql/sql_test.go
@@ -0,0 +1,52 @@
+package orm
+
+import "testing"
+
+func TestGetBulkInsertSqlStr(t *testing.T) {
+	var columns []string
+	var values [][]interface{}
+	columns = append(columns, "`user_name`")
+	columns = append(columns, "`user_age`")
+	columns = append(columns, "`user_sex`")
+
+	var oneColumn []interface{}
+	oneColumn = append(oneColumn, "trump")
+	oneColumn = append(oneColumn, 18.1)
+	oneColumn = append(oneColumn, 0)
+	var twoColumn []interface{}
+	twoColumn = append(twoColumn, "trump2")
+	twoColumn = append(twoColumn, 18.2)
+	twoColumn = append(twoColumn, 1)
+	var threeColumn []interface{}
+	threeColumn = append(threeColumn, "trump3")
+	threeColumn = append(threeColumn, 18.3)
+	threeColumn = append(threeColumn, 2)
+
+	values = append(values, oneColumn)
+	values = append(values, twoColumn)
+	values = append(values, threeColumn)
+
+	type args struct {
+		table   string
+		columns []string
+		values  [][]interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"case1", args{
+			table: "user",
+			columns: columns,
+			values: values,
+		}, ""},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetBulkInsertSqlStr(tt.args.table, tt.args.columns, tt.args.values...); got == tt.want {
+				t.Errorf("GetBulkInsertSqlStr() got empty")
+			}
+		})
+	}
+}
