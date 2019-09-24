43792803e37570de6638943ea2777b650f95f009
liu <Andy.Wei@trump-connext.local>
2019-09-24 10:08:13 +0800

    comment add

--- types/auth/auth_organization.go
+++ types/auth/auth_organization.go
@@ -1,8 +1,13 @@
 package types
 
+//组织类型
 const (
+	//未分组
 	OrganizationTypeNoGrouped = iota
+	//公司
 	OrganizationTypeCompany
+	//子公司
 	OrganizationTypeSumCompany
+	//部门
 	OrganizationTypeDepartment
 )
